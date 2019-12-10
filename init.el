55699329f67e7b144a157b75a13339f383f48cec
Liam Eckert <liamckert17@gmail.com>
2019-12-10 09:43:36 -0500

    Change to init

--- init.el
+++ init.el
@@ -1,7 +1,26 @@
-(defun get-fullpath (@file-relative-path)
-  (concat (file-name-directory (or load-file-name buffer-file-name)) @file-relative-path))
+;;; init.el --- the initial package that leads to everything else
 
-(if (file-exists-p (get-fullpath "user-settings.el"))
-    (load (get-fullpath "/core/loader.el"))
-  (load (get-fullpath "setup.el")))
-		   
+;;; Commentary:
+;; Author: Liam Eckert
+
+;;; Code:
+
+;; Set garbage collection threshold higher for init
+(setq gc-cons-threshold 100000000)
+;; Default garbage collection threshold
+(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 800000)))
+
+(defvar gc-timer
+  (run-with-idle-timer 10 t
+                       (lambda ()
+			 (garbage-collect))))
+
+(setq inhibit-startup-screen t)
+
+(defun get-fullpath (file-relative-path)
+  "Gets path relative to call location of FILE-RELATIVE-PATH."
+  (concat (file-name-directory (or load-file-name buffer-file-name)) file-relative-path))
+
+(load (get-fullpath "./core/loader.el"))
+
+;;; init.el ends here
